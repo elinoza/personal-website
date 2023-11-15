8116657b3eeb32373e15dc262abced08d590f9c1
elinoza <hillcakmak@gmail.com>
2023-11-15 12:30:42 +0300

    projects  animation being done

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -7,7 +7,7 @@ import NavBar from "./NavBar";
 
 class Main extends Component {
   state = {
-    detail_active:false
+    detail_active: false,
   };
 
   render() {
@@ -58,39 +58,118 @@ class Main extends Component {
                 src="https://images.unsplash.com/photo-1481349518771-20055b2a7b24?q=80&w=1239&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="project_1"
               />
-              <div className='overlay'></div>
-              <div className='plus'></div>
-              <div className="detail">
-                <div className="close"></div>
-               <img src="" alt="header-img"/>
-              <div className="website"></div>
-              <div className="infos">
-                <div className="project-Name">The Weather App</div>
-              </div>
-              </div>
+              <div className="overlay"></div>
+              <div
+                className="plus"
+                onClick={() => this.setState({ detail_active: true })}
+              ></div>
+              <div
+                className={
+                  this.state.detail_active ? "detail active" : "detail"
+                }
+              >
+                <div className="close" onclick={()=>this.setState({detail_active:false})}></div>
 
+                <div className="website"></div>
+                <div className="infos">
+                  <img
+                    className="header"
+                    src="https://images.unsplash.com/photo-1518051870910-a46e30d9db16?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt="header-img"
+                  />
+                  <div className="information"></div>
+                  <div className="project-Name">The Weather App</div>
+                </div>
+              </div>
             </Col>
             <Col className="project" xs={12} md={6}>
               <img
                 src="https://images.unsplash.com/photo-1481349518771-20055b2a7b24?q=80&w=1239&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="project_1"
               />
-               <div className='overlay'></div>
+              <div className="overlay"></div>
+              <div
+                className="plus"
+                onClick={() => this.setState({ detail_active: true })}
+              ></div>
+              <div
+                className={
+                  this.state.detail_active ? "detail active" : "detail"
+                }
+              >
+                <div className="close"></div>
+
+                <div className="website"></div>
+                <div className="infos">
+                  <img
+                    className="header"
+                    src="https://images.unsplash.com/photo-1518051870910-a46e30d9db16?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt="header-img"
+                  />
+                  <div className="information"></div>
+                  <div className="project-Name">The Weather App</div>
+                </div>
+              </div>
             </Col>
             <Col className="project" xs={12} md={6}>
               <img
                 src="https://images.unsplash.com/photo-1481349518771-20055b2a7b24?q=80&w=1239&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="project_1"
               />
-               <div className='overlay'></div>
+              <div className="overlay"></div>
+              <div
+                className="plus"
+                onClick={() => this.setState({ detail_active: true })}
+              ></div>
+              <div
+                className={
+                  this.state.detail_active ? "detail active" : "detail"
+                }
+              >
+                <div className="close"></div>
+
+                <div className="website"></div>
+                <div className="infos">
+                  <img
+                    className="header"
+                    src="https://images.unsplash.com/photo-1518051870910-a46e30d9db16?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt="header-img"
+                  />
+                  <div className="information"></div>
+                  <div className="project-Name">The Weather App</div>
+                </div>
+              </div>
             </Col>
             <Col className="project" xs={12} md={6}>
               <img
                 src="https://images.unsplash.com/photo-1481349518771-20055b2a7b24?q=80&w=1239&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="project_1"
               />
-               <div className='overlay'></div>
+              <div className="overlay"></div>
+              <div
+                className="plus"
+                onClick={() => this.setState({ detail_active: true })}
+              ></div>
+              <div
+                className={
+                  this.state.detail_active ? "detail active" : "detail"
+                }
+              >
+                <div className="close"></div>
+
+                <div className="website"></div>
+                <div className="infos">
+                  <img
+                    className="header"
+                    src="https://images.unsplash.com/photo-1518051870910-a46e30d9db16?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt="header-img"
+                  />
+                  <div className="information"></div>
+                  <div className="project-Name">The Weather App</div>
+                </div>
+              </div>
             </Col>
+         
           </Row>
         </Container>
       </>
