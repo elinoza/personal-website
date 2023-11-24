4c86fdfa1598d1809e384ff635c8dcf0d64f84db
elinoza <hillcakmak@gmail.com>
2023-11-24 21:55:31 +0300

    cc

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,73 +1,87 @@
 import React, { Component } from "react";
 
 import "./Main.scss";
-import Details from "./Details.json"
-import { FaInstagram,FaLinkedin } from "react-icons/fa";
-
-
+import Details from "./Details.json";
+import { FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaMedium } from "react-icons/fa6";
+import { SiGmail } from "react-icons/si";
 
 import { Col, Row, Container, Button, Badge } from "react-bootstrap";
-import Projects from  "./Projects"
+import Projects from "./Projects";
 
 import NavBar from "./NavBar";
 
 class Main extends Component {
-  state = {
-   
-
-  };
-
+  state = {};
 
   render() {
-   
-console.log(Details.length)
-
+    console.log(Details.length);
+    let logos =["https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" ,
+     "https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white" ,
+     "https://w7.pngwing.com/pngs/628/224/png-transparent-bootstrap-plain-wordmark-logo-icon-thumbnail.png" ,
+     "https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" ,
+     "https://cdn0.iconfinder.com/data/icons/logos-brands-in-colors/128/react-512.png" ,
+     "https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white" ,
+     "https://img.shields.io/badge/Microsoft_Azure-0089D6?style=for-the-badge&logo=microsoft-azure&logoColor=white" ,
+     "https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" ,
+     "https://img.shields.io/badge/Express.js-404D59?style=for-the-badge" ,
+     "https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white" ,
+     "https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white" ,
+     "https://img.shields.io/badge/Heroku-430098?style=for-the-badge&logo=heroku&logoColor=white" ,
+    "https://www.svgrepo.com/show/452093/redux.svg"]
     return (
       <>
         <Container>
+    
           <NavBar />
-          <Row >
+          <Row className="first-row">
             <Col xs={12} md={6}>
               <div className="my-jumbotron">
-                <h1>Hi! I'm Hilal  </h1>
-                <p>
-                  a Full Stack Developer{" "}
-                  <Badge bg="warning">Frontend/Backend</Badge> with a passion
-                  for building awesome web applications!
-                </p>
+                <h1>Hi! I'm Hilal   </h1>
+<div className="jumbotron-body"><div className="sliding-wrapper ">
+                  {" "}
+                  <div className="pre d-inline">
+                    I'm a{" "}
+                    <div className="primary-sliding-wrapper">
+                      {" "}
+                      full-stack{" "}
+                      <div className="blank-div">
+                        <div className="sliding-parent">
+                          <div className="sliding-element"> Frontend</div>
+                          <div className="sliding-element"> Backend</div>
+                          <div className="sliding-element"> Full-Stack</div>
+                        </div>
+                      </div>{" "}
+                    </div>{" "}
+                    Developer 
+                  </div>{" "}
+                  
+                </div>
+                <p className="d-inline">I'm passionate for building awesome web applications! I'm an industrial/System Engineer.I successfully completed a 6-months Full Stack (MERN) Developer Bootcamp Program in Epicode (formerly known as Strive School GmBh).
+ </p></div>
+                
 
-                <div className="d-flex justify-content-center">
-                <FaInstagram />
-                <FaLinkedin />
+                <div className="d-flex justify-content-end">
+                  <SiGmail className="icon" />
+                  <FaLinkedin className="icon" />
+                  <FaMedium className="icon" />
                 </div>
               </div>
             </Col>
             <Col className="skills-section my-jumbotron" xs={12} md={6}>
+             
               <h1> SKILLS</h1>
               <img src="./profil.png" />{" "}
               <div id="skill-set">
-                {" "}
-                <img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white" />{" "}
-                <img src="https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white" />{" "}
-                <img src="https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white" />{" "}
-                <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" />{" "}
-                <img src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB" />{" "}
-                <img src="https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white" />{" "}
-                <img src="https://img.shields.io/badge/Microsoft_Azure-0089D6?style=for-the-badge&logo=microsoft-azure&logoColor=white" />{" "}
-                <img src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" />{" "}
-                <img src="https://img.shields.io/badge/Express.js-404D59?style=for-the-badge" />{" "}
-                <img src="https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white" />{" "}
-                <img src="https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white" />{" "}
-                <img src="https://img.shields.io/badge/Heroku-430098?style=for-the-badge&logo=heroku&logoColor=white" />
+            {logos.map((elem)=> <div className="skill-icon"><img src={elem}/></div>)}
               </div>
             </Col>
           </Row>
           <h1>My Projects</h1>
           <Row id="projects-row">
-{ Details.map((detail,index)=><Projects key={index} detail={detail}  />)}
-        
-         
-         
+            {Details.map((detail, index) => (
+              <Projects key={index} detail={detail} />
+            ))}
           </Row>
           <div>Footer</div>
         </Container>
--- src/Components/Main/Navbar.jsx
+++ src/Components/Main/Navbar.jsx
@@ -1,34 +1,40 @@
-import React, {Component} from "react";
-import "./Main.css"
-import { Navbar,Nav,NavDropdown,Container,Button} from "react-bootstrap"
+import React, { Component } from "react";
+import "./Main.css";
+import { Navbar, Nav, NavDropdown, Container, Button } from "react-bootstrap";
 
-
-class NavBar extends Component
-{
-    state={}
-    render()
-    {
-        return(
+class NavBar extends Component {
+  state = {};
+  render() {
+    return (
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">
+            {" "}
+            <div className="animated-eye">
+              {" "}
+              <div class="eye-lid-top"> </div>
+              <div class="eye-lid-bottom"> </div>
+              <div class="eye">
+                <div class="iris">
+                  <div class="pupil"></div>
+                </div>
+              </div>
             
-           <Navbar expand="lg" className="bg-body-tertiary">
-           <Container>
-             <Navbar.Brand href="#home"> Hilal  Semercioğlu. </Navbar.Brand>
-             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-             <Navbar.Collapse id="basic-navbar-nav">
-               <Nav className="me-auto">
-                 <Nav.Link href="#home">About</Nav.Link>
-                 <Nav.Link href="#link">Projects</Nav.Link>
-                 <Nav.Link className="download_cv" href="#link">Download CV</Nav.Link>
-            
-               </Nav>
-             </Navbar.Collapse>
-           </Container>
-         </Navbar>
-     
-
-        )
-
-    }
-
+            </div>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">About</Nav.Link>
+              <Nav.Link href="#link">Projects</Nav.Link>
+              <Nav.Link className="download_cv" href="#link">
+                Download CV
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
 }
- export default NavBar;
+export default NavBar;
